646ede2892f6a3594467e3f0d15c072530f3c9ae
thinhmascot <67301649+thinhmascot@users.noreply.github.com>
2021-05-02 18:08:23 +0700

    asdasd

--- PdfRawImg/Program.cs
+++ PdfRawImg/Program.cs
@@ -11,11 +11,13 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
+using TeamDev.Redis;
 
 namespace PdfRawImg
 {
     public class ModuleFilePdf : HttpModule
     {
+        const int MAX_ = 5;
         static ConcurrentDictionary<string, byte[]> m_files = new ConcurrentDictionary<string, byte[]>() { };
         public override bool Process(IHttpRequest request, IHttpResponse response, IHttpSession session)
         {
@@ -38,7 +40,9 @@ namespace PdfRawImg
                                     key = DocumentStatic.buildId(pageTotal, new FileInfo(file).Length);
 
                                     int dpiX = 96, dpiY = 96;
-                                    for (int i = 0; i < pageTotal; i++)
+                                    int max = pageTotal;
+                                    if (pageTotal > MAX_) max = MAX_; else max = pageTotal;
+                                    for (int i = 0; i < max; i++)
                                     {
                                         var p = doc.PageSizes[i];
                                         using (var image = doc.Render(i, (int)p.Width, (int)p.Height, dpiX, dpiY, false))
@@ -75,17 +79,16 @@ namespace PdfRawImg
                         break;
                     case "/clear":
                         key = request.QueryString["key"].Value;
-                        if (!string.IsNullOrEmpty(key))
+                        if (string.IsNullOrEmpty(key))
+                            m_files.Clear();
+                        else
                         {
                             var arr = m_files.Keys.Where(x => x.Contains(key)).ToArray();
-
-                            if (m_files.TryGetValue(imgName, out buffer) && buffer.Length > 0)
-                            {
-                                response.Status = HttpStatusCode.OK;
-                                response.ContentType = "image/jpeg";
-                                response.Body.Write(buffer, 0, buffer.Length);
-                            }
+                            foreach (var name in arr) m_files.TryRemove(name, out byte[] bf);
                         }
+                        response.Status = HttpStatusCode.OK;
+                        buffer = Encoding.ASCII.GetBytes("OK");
+                        response.Body.Write(buffer, 0, buffer.Length);
                         break;
                 }
             }
@@ -99,17 +102,30 @@ namespace PdfRawImg
         }
     }
 
+    public class myEntity
+    {
+        [DocumentStoreKey]
+        public string mykeyproperty { get; set; }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var redis = new RedisDataAccessProvider();
+            redis.Configuration.Host = "127.0.0.1";
+            redis.Configuration.Port = 1000;
+            redis.Connect();
+
+            //redis.SendCommand(RedisCommand.SET, "mykey", "myvalue");
+
+            redis.WaitComplete();
+
+
             HttpServer server = new HttpServer();
             server.Add(new ModuleFilePdf());
             server.Start(IPAddress.Any, 12345);
             Console.ReadLine();
-
         }
-
     }
 }
